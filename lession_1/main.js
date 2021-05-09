512ccf3d77f7a3e1e62a5484c7256cf0318b18dc
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-05-09 23:53:29 +0700

    Update main.js

--- lession_1/main.js
+++ lession_1/main.js
@@ -1,5 +1,10 @@
 
-var fullName = 'Tran Xuan Phuc';
-var age = 24;
-alert(fullName);
-alert(age);
+// Làm bài tập tại đây
+fullName = 'Tran Xuan Phuc'
+alert(fullName)
+message = 'Học về built-in functions trong JS'
+warnMessage = 'Đây là message cảnh báo'
+errorMessage = 'Đây là message lỗi'
+console.log(message)
+console.warn(warnMessage)
+console.error(errorMessage)
